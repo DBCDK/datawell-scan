801b2405956a2f5cb982ba83f72dc28b387560c4
Morten Bøgeskov <mb@dbc.dk>
2019-07-05 11:19:24 +0200

    Wrap profile service

---
+++ profile-change/common/src/test/java/dk/dbc/scan/common/ProfileServiceActionsIT.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of datawell-scan-profile-change-monitor
+ *
+ * datawell-scan-profile-change-monitor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * datawell-scan-profile-change-monitor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.scan.common;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Morten Bøgeskov (mb@dbc.dk)
+ */
+public class ProfileServiceActionsIT {
+
+    @Test(timeout = 2_000L)
+    public void testGetProfile() throws Exception {
+        System.out.println("testGetProfile");
+
+        ProfileServiceActions psa = new ProfileServiceActions("http://localhost:" + System.getProperty("wiremock.port", "80") + "/profile-service/");
+
+        Profile profile = psa.getProfile("102030-danbib");
+        assertThat(profile.contains("800000-danbib"), is(true));
+        assertThat(profile.contains("870970-danbib"), is(true));
+    }
+
+}
---
+++ profile-change/common/src/test/java/dk/dbc/scan/common/ProfileServiceActionsTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2019 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of datawell-scan-profile-change-monitor
+ *
+ * datawell-scan-profile-change-monitor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * datawell-scan-profile-change-monitor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.scan.common;
+
+import java.net.URI;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Morten Bøgeskov (mb@dbc.dk)
+ */
+public class ProfileServiceActionsTest {
+
+    @Test(timeout = 2_000L)
+    public void testUri() throws Exception {
+        System.out.println("testUri");
+
+        ProfileServiceActions psa = new ProfileServiceActions("http://localhost:12345");
+
+        URI uri = psa.getUri(123456, "abc");
+        System.out.println("uri = " + uri);
+        assertThat(uri.toString(), is("http://localhost:12345/api/profile/123456/abc"));
+    }
+
+}
